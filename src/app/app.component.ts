e2e7d3b3f10068a0653fa624ff7c405f82a86bb5
Younes Hashlamoun <Younes.Hashlamoun@caliberhomeloans.com>
2022-05-25 19:00:16 -0500

    section6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedChoice = 'recipe';
+
+  onNavigate(choice: string) {
+    this.loadedChoice = choice;
+  }
 }
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() displayFlag = new EventEmitter<string>();
 
+  onSelect(choice: string) {
+    this.displayFlag.emit(choice);
+    // console.log(e);
+    // console.log(e.target);
+    // var tag = (<HTMLAnchorElement>e.target).tagName.toLowerCase();
+
+    // console.log(tag.toUpperCase());
+
+  }
 }
